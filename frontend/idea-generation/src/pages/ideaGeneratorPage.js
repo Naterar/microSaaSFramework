f3ec093415c7f5843e32a26df5b73e60491e64bd
Roberto Natera <ohana2116@gmail.com>
2024-01-15 18:28:08 -0500

    Improving styling to the Idea Generator; file updated: ideaGeneratorPage.js

--- frontend/idea-generation/src/pages/ideaGeneratorPage.js
+++ frontend/idea-generation/src/pages/ideaGeneratorPage.js
@@ -6,23 +6,33 @@ const IdeaGeneratorPage = () => {
 
   return (
     <div>
-      <div className="">
-        <div>Create Your Micro SaaS Idea</div>
-        <input
-          className="bg-gray-400 text-black"
-          type="text"
-          placeholder=""
-          onChange={(e) => setTargetAudience(e.target.value)}
-          value={targetAudience}
-        ></input>
-        <input
-          className="bg-gray-400 text-black"
-          type="text"
-          placeholder=""
-          onChange={(e) => setInterest(e.target.value)}
-          value={interest}
-        ></input>
-        <div className="">Button</div>
+      <div className="w-full my-20 flex flex-col gap-4 max-w-lg mx-auto">
+        <div className="text-3xl text-center font-bold">
+          Create Your Micro SaaS Idea
+        </div>
+        <div>
+          <div className="font-semibold my-2">Target Audience</div>
+          <input
+            className="bg-gray-400 text-black w-full placeholder-white"
+            type="text"
+            placeholder="Shopify Merchants"
+            onChange={(e) => setTargetAudience(e.target.value)}
+            value={targetAudience}
+          ></input>
+        </div>
+        <div>
+          <div className="font-semibold my-2">Interest</div>
+          <input
+            className="bg-gray-400 text-black w-full placeholder-white"
+            type="text"
+            placeholder="AI SEO Tools"
+            onChange={(e) => setInterest(e.target.value)}
+            value={interest}
+          ></input>
+        </div>
+        <div className="py-2 px-2 items-center font-semi-bold w-full text-md bg-pink-800 hover:bg-pink-600 rounded-md text-center ">
+          Generate Description
+        </div>
       </div>
     </div>
   );
